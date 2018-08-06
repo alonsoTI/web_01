e44fbc52fc262d31fed926e3543b139f1e0bbc68
Victor Alonso <alonso.victoer38@gmail.com>
2018-08-06 07:15:28 -0500

    Avance al 50%

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $('#btn-menu').click(function(){
         if($('.btn-menu span').attr('class') == 'fa fa-bars'){
-            $('.btn-menu span').removeClass('fa fa-bars').addClass('fa fa-close');
+            $('.btn-menu span').removeClass('fa fa-bars').addClass('fa fa-industry');
             $('.menu-link').css({
                 'left':'0'
             })
         }else{
-            $('.btn-menu span').removeClass('fa fa-close').addClass('fa fa-bars');
+            $('.btn-menu span').removeClass('fa fa-industry').addClass('fa fa-bars');
             $('.menu-link').css({
                 'left':'-100%'
             })
